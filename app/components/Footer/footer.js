09ac2b730d60b66048f9850ce83c975fcd73fe2e
raghav <raghav09.dude@gmail.com>
2024-08-15 14:36:28 +0530

    added UI for HomePage added about/contact page

---
+++ app/components/Footer/footer.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+const Footer = () => (
+    <footer className="footer_container">
+        <p>&copy; 2024 Charity Organization</p>
+    </footer>
+)
+
+export default Footer
--- app/page.js
+++ app/page.js
@@ -1,8 +1,36 @@
+import Image from 'next/image';
+import Head from "next/head";
+import Link from "next/link";
+
+import './style.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Jan Kalyan Seva
-    </main>
+      <main className="home_container">
+          <div className="home_wrapper">
+              <Head>
+                  <meta name="description" content="Support our cause"/>
+              </Head>
+              <header className="home_header">
+                  <h1>Welcome to Jan Kalyan Sewa</h1>
+              </header>
+              <main>
+                  <p className="home_description">Details about the charity’s mission and goals.</p>
+              </main>
+              <button className="home-btn">
+                  <Link href="/about">Find out more</Link>
+              </button>
+          </div>
+          <Image
+              src="/poster.webp"
+              layout="responsive"
+              width={500}
+              height={500}
+              alt="poster" className="home_poster"
+              fill={false}
+              style={{ objectFit: "contain" }}
+
+          />
+      </main>
   );
 }
